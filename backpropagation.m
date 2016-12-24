31eea36cafe44ac069c9dbad854192603cb35b63
Daniel Materka <daniel.materka@gmail.com>
2016-12-24 18:32:12 +0000

    multilayer prepare code

--- backpropagation.m
+++ backpropagation.m
@@ -7,15 +7,22 @@ deltaoutput = diffsum .* deroutput;
 avgdiff(it) = mean(abs(diffsum));
 
 for y = 1:numoutputs
-    for x = 1:numhidden
-        tmp = ( deltaoutput / hiddens(1,x).value );
+    for x = 1:hiddensdef(1)
+        tmp = ( deltaoutput / hiddens(end,x).value );
         outputs(y).bonds(x) = outputs(y).bonds(x) - tmp;
-        hiddens(1,x).sum = (deltaoutput / outputs(y).bonds(x)) .* sigmoid_prime(hiddens(1,x).value,1,0);
+        hiddens(end,x).sum = (deltaoutput / outputs(y).bonds(x)) .* sigmoid_prime(hiddens(end,x).value,1,0);
     end
 end
-% Recalculating initial weights
 
-for y = 1:numhidden
+% code for adding inter-multilayer calculations
+
+
+
+%
+
+% Recalculating initial weights
+% mhl won't change due to multilayers
+for y = 1:hiddensdef(1)
     for x = 1:size(inputs,1)
         hiddens(1,y).bonds(x) = hiddens(1,y).bonds(x) - (hiddens(1,y).sum / inputs(x,:));
     end
