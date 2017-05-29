5d597a56bc2d738714047c16914aa3d0d42ee1a1
Daniel Materka <daniel.materka@gmail.com>
2017-05-29 23:58:44 +0100

    Make the network operating on integers, not vectors

--- neur.m
+++ neur.m
@@ -1,40 +1,42 @@
 %%%% script starts %%%%
 it = 1;
-numit = 10000;
+numit = 100;
 numoutputs = 1;
 numhiddenlayers = 1;
-hiddensdef = [8];
+hiddensdef = [3];
 hiddens = struct();
 outputs = struct();
-inputs = [0,1,0,1; 
-          1,1,0,0];
-expected=[1,0,0,1];
+inputs = [0.01;  
+          0.99];
+expected=[0.01];
 
 %%%% allocate output variable %%%%
-out = zeros(numit,length(inputs));
+out = zeros(numit,length(outputs));
 
 %%%% initialize weights %%%% 
 for z = length(hiddensdef)
     for y = 1:hiddensdef(z)
-        hiddens(1,y).bonds = rand(size(inputs,1),1);
+        hiddens(z,y).bonds = rand(size(inputs,1),1);
     end
 end
 
 for y = 1:numoutputs
+    %%output bonds conformed with hiddensdef bonds number
     outputs(y).bonds = rand(1,hiddensdef(end));
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 for it = 1:numit
 init_neur
 backpropagation
 end
 out(end,:)'
-plot(1:it,out(:,:)')
+plot(1:it,out(:,:)');
 hold on
-p1 = plot(avgdiff,'r')
+p1 = plot(avgdiff,'r');
 hold off
 bonds_out = outputs.bonds;
 bonds_hidden = hiddens.bonds;
 save('neural_xor','outputs','hiddens','hiddensdef')
-p2 = plot(avgdiff)
+p2 = plot(avgdiff);
 %clear;
