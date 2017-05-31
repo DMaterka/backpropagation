3b671a0a3f9cba41cb6533c4be663ca0540cc4be
Daniel Materka <daniel.materka@gmail.com>
2017-05-31 23:06:43 +0100

    Backpropagation bugs fixing

--- backpropagation.m
+++ backpropagation.m
@@ -5,26 +5,23 @@ deroutput = sigmoid_prime(outputs(1).value,1,0);
 
 deltaoutput = diffsum .* deroutput;
 avgdiff(it) = mean(abs(diffsum));
-
+doutputsum = 0;
+weightdiff =0;
 for y = 1:numoutputs
+    sumoutp = 0;
+    doutputsum = sigmoid_prime(outputs(y).sum,1,0) * (diffsum);
     for x = 1:hiddensdef(1)
-        tmp = ( deltaoutput / hiddens(end,x).value );
-        outputs(y).bonds(x) = outputs(y).bonds(x) - tmp;
-        hiddens(end,x).sum = (deltaoutput / outputs(y).bonds(x)) .* sigmoid_prime(hiddens(end,x).value,1,0);
+        weightdiff = doutputsum * hiddens(1,x).value;
+        outputs(y).bonds(x) = outputs(y).bonds(x) - weightdiff ;
     end
 end
 
-% code for adding inter-multilayer calculations
 
-
-
-%
-
-% Recalculating initial weights
-% mhl won't change due to multilayers
 for y = 1:hiddensdef(1)
+    dhiddensum =0;
+    dhiddensum = doutputsum * outputs(1).bonds(y) * sigmoid_prime(hiddens(1,y).sum,1,0);
     for x = 1:size(inputs,1)
-        hiddens(1,y).bonds(x) = hiddens(1,y).bonds(x) - (hiddens(1,y).sum / inputs(x,:));
+        weightdiff = dhiddensum * inputs(x,1);
+        hiddens(1,y).bonds(x) = hiddens(1,y).bonds(x) - dhiddensum ;
     end
 end
-
